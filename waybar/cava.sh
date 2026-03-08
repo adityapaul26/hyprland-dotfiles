da870f3d07f492209adc6482d9e8a1bfe731abf8
adityapaul26 <adityapaul2603@gmail.com>
2026-03-08 12:25:47 +0530

    cava update

--- waybar/cava.sh
+++ waybar/cava.sh
@@ -1,6 +1,54 @@
+##!/bin/bash
+#
+## Create a temporary config file for cava
+#CAVA_CONFIG="/tmp/waybar_cava_config"
+#echo "
+#[general]
+#bars = 10
+#sleep_timer = 0
+#
+#[output]
+#method = raw
+#raw_target = /dev/stdout
+#data_format = ascii
+#ascii_max_range = 7
+#" >"$CAVA_CONFIG"
+#
+## Kill any existing cava process
+#pkill -f "cava -p $CAVA_CONFIG"
+#
+## Run cava and use awk to handle output
+## It will output an empty string (hiding the module) when silent
+#cava -p "$CAVA_CONFIG" | awk -F ';' '{
+#    # Check if all bars are zero
+#    total = 0
+#    for (i=1; i<NF; i++) total += $i
+#
+#    if (total == 0) {
+#        print ""
+#    } else {
+#        res = ""
+#        for (i=1; i<NF; i++) {
+#            v = $i
+#            if (v == 0) res = res " "
+#            else if (v == 1) res = res "▂"
+#            else if (v == 2) res = res "▃"
+#            else if (v == 3) res = res "▄"
+#            else if (v == 4) res = res "▅"
+#            else if (v == 5) res = res "▆"
+#            else if (v == 6) res = res "▇"
+#            else res = res "█"
+#        }
+#        print res
+#    }
+#    fflush()
+#}'
 #! /bin/bash
 
+sleep 2
+
 bar="▁▂▃▄▅▆▇█"
+
 dict="s/;//g;"
 
 # creating "dictionary" to replace char with bar
